b4682b5c0bc3fd99874c378ef6cc53f3226ac4e7
TetianaHumeniuk <Tetiana.Humeniuk.ShI.2023@lpnu.ua>
2025-04-10 18:35:55 +0300

    save data for db in .env

--- nextdoor/settings.py
+++ nextdoor/settings.py
@@ -76,14 +76,16 @@ WSGI_APPLICATION = 'nextdoor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+from decouple import config
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'clubs_db',
-        'USER': 'root',
-        'PASSWORD': '', #your password
-        'HOST': 'localhost',
-        'PORT': '3306', 
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='3306'),
     }
 }
 
